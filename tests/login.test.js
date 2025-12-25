ce3914bd98a55a2c06899b101af9253fed321e22
Rodrigo <rdgo_silva@hotmail.com>
2025-12-25 18:51:15 -0300

    Adicionando o arquivo inicial com o teste de login

---
+++ tests/login.test.js
@@ -0,0 +1,35 @@
+import http from 'k6/http';   //importa o modulo
+import { sleep, check } from 'k6';  // importa a função
+
+export const options = {
+  iterations: 10,
+  	thresholds: {
+		http_req_duration: ['p(90)<10', 'max<1'], // p90 tem que ser menor do que 10ms
+		http_req_failed: ['rate<0.01'] // falhas menor do que 1%
+	}
+};
+
+export default function () {
+	const url = 'https://localhost:3000/login';
+	
+    const payload = JSON.stringify({
+	    username:'julio.lima',
+	    senha: '123456'           // isso é um objeto javascript. P/converter em uma string json utiliza o JSON.stringify
+    });
+
+ const params = {
+	headers: {
+		'Content-Type': 'application/json',
+
+
+		},
+}; 
+
+const res = http.post(url, payload,params);
+check(res, {  // podem haver várias validações aqui dentro
+	'Validar que o status é 200': (r) => r.status === 200,   // o r poderia ser qq coisa
+	'Validar que o token é string': (r) => typeof(r.json().token) == 'string'
+})
+
+sleep(1);
+}
